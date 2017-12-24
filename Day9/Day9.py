805fbc2b8009b5b28364114ef6718daf1fcf1e8d
arvindc95 <arvind_95@hotmail.com>
2017-12-24 15:01:14 -0500

    Day 9 star 1

---
+++ Day9/Day9.py
@@ -0,0 +1,31 @@
+def day9star1():
+    f = open('inputDay9Star1.txt')
+    chars = f.readline()
+    score = 0
+    groups = []
+    i = 0
+    while i < len(chars):
+        if chars[i] == '!':
+            i += 2 # could throw index error
+        elif chars[i] == '<':
+            while chars[i] != '>':
+                if chars[i] == '!':
+                    i += 2 # could throw index error
+                else:
+                    i += 1
+        elif chars[i] == '{':
+            groups.append(chars[i])
+            i += 1
+        elif chars[i] == '}':
+            score += len(groups)
+            groups.pop() # does popping empty list throw error?
+            i += 1
+        else:
+            i += 1
+    print("Score:", score)
+        
+        
+        
+# def day9star2():
+
+day9star1()
